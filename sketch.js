145898e05247025fb5c05af75155ea8e0a95450f
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2018-01-24 15:25:54 +0000

    updated starter code to be clearer for students

--- sketch.js
+++ sketch.js
@@ -1,44 +1,17 @@
-var lpkidsred;
-var lpkidsgreen;
-var lpkidsblue;
-var lpkidsgray;
+var redPicture;
+var greenPicture;
+var bluePicture;
+var grayPicture;
 var television;
 
 function preload(){
-    lpkidsgray = loadImage("images/lpkids-gray.png");
-    lpkidsred = loadImage("images/lpkids-red.png");
-    lpkidsgreen = loadImage("images/lpkids-green.png");
-    lpkidsblue = loadImage("images/lpkids-blue.png");
-    television = loadImage("images/old-tv.png");
+
 }
 
 function setup(){
-    createCanvas(749,524);
+    createCanvas(500,374);
 }
 
 function draw(){
-    if(keyIsPressed){
-        if(key=="r"){
-            image(lpkidsred, 50, 50);
-        }
-        if(key=="g"){
-            image(lpkidsgreen, 50, 50);
-        }
-        if(key=="b"){
-            image(lpkidsblue, 50, 50);
-        }
-        if(keyCode=="32"){
-            fill(0);
-            rect(50, 50, 500, 374);
-            blendMode(LIGHTEST);
-            image(lpkidsred,50,50);
-            image(lpkidsgreen,50,50);
-            image(lpkidsblue,50,50);
-        }
-    }
-    else{
-        blendMode(NORMAL);
-        image(lpkidsgray,50, 50);
-        image(television, 0, 0);
-    }
+    
 }
